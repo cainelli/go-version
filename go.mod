4b78ad5b8ed661f5efc8463f1865dd0f0353e592
Fernando Cainelli <fernando.cainelli@getyourguide.com>
2023-03-31 10:55:27 -0300

    release v1.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cainelli/go-version
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,19 @@
+package goversion
+
+import "fmt"
+
+func String() string {
+	return fmt.Sprintf("v%s.%s.%s", Major(), Minor(), Patch())
+}
+
+func Major() string {
+	return "1"
+}
+
+func Minor() string {
+	return "0"
+}
+
+func Patch() string {
+	return "0"
+}
